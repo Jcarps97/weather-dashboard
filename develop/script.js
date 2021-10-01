436595a997642f841f65725c4c121d15cb24ac84
Jack Carpenter <jackcarpenter1@live.com>
2021-10-01 15:19:17 -0500

    Routine update on code

--- develop/script.js
+++ develop/script.js
@@ -12,33 +12,55 @@
 var searchBtn = $(".searchBtn");
 var cityName;
 var apikey = "4846f5792a31d91f265afac91672a4b8";
+var mainUVI = $(".uvi")
 var mainTemp = $(".temp")
 var mainWind = $(".wind")
 var mainHum = $(".humidity")
+var currentDay = moment()
 
 
 //Calling the API
 function getAPI(location) {
-     var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${apikey}`; 
+    var apiUrl1 = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${apikey}`; 
                 
-    fetch(apiUrl)
+    fetch(apiUrl1)
         .then(function (response) {
                 return response.json();
         })
         .then(function (data) {
-            // console.log(data.wind.speed);
-            // console.log(data.main.temp);
-            // console.log(data.main.humidity);
-            mainTemp.text(data.main.temp);
-            mainWind.text(data.wind.speed + "mph");
-            mainHum.text(data.main.humidity);
+            // console.log(data)
+            var lat = data.coord.lat
+            var long = data.coord.lon
+            console.log(lat)
+            console.log(long)
+        
 
+
+        displayData(lat, long);
         });
         
 }
 
 //Displaying fetched data
-function displayData() {
+function displayData(lat, long) {
+    var apiUrl2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&units=imperial&appid=" + apikey ;
+    fetch(apiUrl2)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data)
+            mainTemp.text("Temperature " + data.current.temp + " F");
+            mainWind.text("Wind " + data.current.wind_speed + "mph");
+            mainHum.text("Humidity " + data.current.humidity + "%");
+            mainUVI.text("UV Index " + data.current.uvi);
+        $(".forecast").each(function(index){
+            text(data.daily[i].current.temp + "F")
+            text(data.daily[i].current.wind_speed + "mph")
+            text(data.daily[i].current.humidity + "%")
+        });
+
+    });
 
 }
 
@@ -50,3 +72,5 @@ searchBtn.on("click", function(){
         
     getAPI()
     })
+
+$(".currentDay").text(currentDay)
